47f66579363ab0e057bc392f441944224419697d
Lukylix <lucas.garcia.bobi@gmail.com>
2020-08-06 00:16:09 +0200

    :sparkles: Themed View & Text

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
+
+// Use our themed variants instead of react-native
+import { Text, View } from "./UI/components/Themed";
+
 // Component to Make sure we arent on the device bar, notches ect..
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
@@ -18,7 +22,6 @@ export default function App() {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: "#fff",
 		alignItems: "center",
 		justifyContent: "center",
 	},
---
+++ UI/components/Themed.tsx
@@ -0,0 +1,40 @@
+import * as React from "react";
+import { Text as DefaultText, View as DefaultView, useColorScheme } from "react-native";
+
+import Colors from "../styles/Colors";
+
+export function useThemeColor(
+	props: { light?: string; dark?: string },
+	//create a type definitions based on Colors.ts
+	colorName: keyof typeof Colors.light & keyof typeof Colors.dark
+) {
+	const theme = useColorScheme() || "light";
+	const colorFromProps = props[theme];
+
+	// if lightColor or darkColor prop as been passed
+	if (colorFromProps) return colorFromProps;
+
+	return Colors[theme][colorName];
+}
+
+type ThemeProps = {
+	lightColor?: string;
+	darkColor?: string;
+};
+
+export type TextProps = ThemeProps & DefaultText["props"];
+export type ViewProps = ThemeProps & DefaultView["props"];
+
+export function Text(props: TextProps) {
+	const { style, lightColor, darkColor, ...otherProps } = props;
+	const color = useThemeColor({ light: lightColor, dark: darkColor }, "text");
+
+	return <DefaultText style={[{ color }, style]} {...otherProps} />;
+}
+
+export function View(props: ViewProps) {
+	const { style, lightColor, darkColor, ...otherProps } = props;
+	const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, "background");
+
+	return <DefaultView style={[{ backgroundColor }, style]} {...otherProps} />;
+}
